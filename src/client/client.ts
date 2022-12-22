9eeb72c9fdd4fb2637c397e5e8763e76ab0a5471
Your Name <you@example.com>
2022-12-22 23:11:28 +0000

    Basic tweaks

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,21 +1,22 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { MapControls, OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 2
+
+camera.position.set( 40, 20, 0 );
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new MapControls(camera, renderer.domElement)
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
-    wireframe: true,
+    wireframe: false,
 })
 
 const cube = new THREE.Mesh(geometry, material)
@@ -32,8 +33,8 @@ function onWindowResize() {
 function animate() {
     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+    cube.rotation.x += 0.0001
+    cube.rotation.y += 0.001
 
     controls.update()
 
