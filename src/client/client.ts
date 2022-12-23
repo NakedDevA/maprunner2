15f32eceb6f8e9ba66cf359f2f268805b2b67a08
Your Name <you@example.com>
2022-12-23 00:24:25 +0000

    Merge meshes for GIGA speedup

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,21 +1,21 @@
 import * as THREE from 'three'
 import { MapControls, OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils'
 
 type LandmarkCoords = {
-    x:number
-    y:number
-    z:number
+    x: number
+    y: number
+    z: number
 }
-const testJson:LandmarkCoords[] = require('./data/level_us_01_01.pak.json')
+const testJson: LandmarkCoords[] = require('./data/level_us_01_01.pak.json')
 
 const scene = new THREE.Scene()
 
-
 scene.background = new THREE.Color(0x444444)
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000)
-camera.position.set( 400, 200, 0 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 3000)
+camera.position.set(400, 200, 0)
 
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
+const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
@@ -30,56 +30,31 @@ controls.minDistance = 10
 controls.maxDistance = 999
 
 controls.maxPolarAngle = Math.PI / 2
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: false,
-})
 
-console.log(testJson)
 // world
 
-const landmarkGeometry = new THREE.BoxGeometry(1, 1, 1)
-landmarkGeometry.translate(0, 0.5, 0)
-const landmarkMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: false })
+const landmarkMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true })
 
+var mergedGeoms = []
 for (const lmk of testJson) {
-    const mesh = new THREE.Mesh(landmarkGeometry, landmarkMaterial)
-    //const point = new THREE.Points(landmarkGeometry, landmarkMaterial)
-    mesh.position.x = lmk.x
-    mesh.position.y = lmk.y
-    mesh.position.z = lmk.z
-    mesh.scale.x = 1
-    mesh.scale.y = 1
-    mesh.scale.z = 1
-    mesh.updateMatrix()
-    mesh.matrixAutoUpdate = false
-    scene.add(mesh)
+    var newBox1 = new THREE.BoxGeometry(2, 2, 2)
+    newBox1.translate(lmk.x, lmk.y, lmk.z)
+    mergedGeoms.push(newBox1)
 }
-
-/*
-for (let i = 0; i < 500; i++) {
-    const mesh = new THREE.Mesh(landmarkGeometry, landmarkMaterial)
-    mesh.position.x = Math.random() * 1600 - 800
-    mesh.position.y = 0
-    mesh.position.z = Math.random() * 1600 - 800
-    mesh.scale.x = 20
-    mesh.scale.y = Math.random() * 80 + 10
-    mesh.scale.z = 20
-    mesh.updateMatrix()
-    mesh.matrixAutoUpdate = false
-    scene.add(mesh)
-}*/
+var mergedBoxes = BufferGeometryUtils.mergeBufferGeometries(mergedGeoms)
+const mesh = new THREE.Mesh(mergedBoxes, landmarkMaterial)
+mesh.updateMatrix()
+mesh.matrixAutoUpdate = false
+scene.add(mesh)
 
 // lights
-
 const dirLight1 = new THREE.DirectionalLight(0xffffff)
 dirLight1.position.set(1, 1, 1)
 scene.add(dirLight1)
 
-const dirLight2 = new THREE.DirectionalLight(0x002288)
+const dirLight2 = new THREE.DirectionalLight(0xc4a704)
 dirLight2.position.set(-1, -1, -1)
-//scene.add(dirLight2)
+scene.add(dirLight2)
 
 const ambientLight = new THREE.AmbientLight(0x222222)
 scene.add(ambientLight)
@@ -94,9 +69,7 @@ function onWindowResize() {
 
 function animate() {
     requestAnimationFrame(animate)
-
     controls.update()
-
     render()
 }
 
