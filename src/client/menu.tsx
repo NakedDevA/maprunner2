e1128fd920726b3bfba51e854040dd3bee8e4e6e
Your Name <you@example.com>
2023-01-03 19:45:28 +0000

    Fitler the list

--- src/client/menu.tsx
+++ src/client/menu.tsx
@@ -7,37 +7,44 @@ const renderer = new CommonDOMRenderer()
 
 export const setZoneMenu = async (path: string, goToZone: (zoneName: string) => void) => {
     const zoneJson = await fetchJson<MapZonesJson>(path)
-    const zoneIDList = Object.keys(zoneJson.zoneDesc)
+    const zoneIDList = Object.keys(zoneJson.zoneDesc).sort()
     const zoneListElement = document.getElementById('zoneList')
     if (!zoneListElement) return
 
     const jsx = (
-        <ul>
-            {zoneIDList.map((zoneId) => {
-                const thisInfo = zoneDescription(zoneJson.zoneDesc[zoneId].props)
-                return (
-                    <li>
-                        <button
-                            onclick={() => {
-                                toggleInfoBox(zoneId)
-                                goToZone(zoneId)
-                            }}
-                        >
-                            {zoneId}
-                        </button>
-                        <p id={`infobox-${zoneId}`} class={'zoneInfoBox'}>
-                            {thisInfo}
-                        </p>
-                    </li>
-                )
-            })}
-        </ul>
+        <>
+            <input
+                type={Text}
+                placeholder={'filter'}
+                oninput={(e: { target: HTMLInputElement }) =>
+                    filterZones(e.target.value, zoneIDList)
+                }
+            ></input>
+            <ul>
+                {zoneIDList.map((zoneId) => {
+                    const thisInfo = zoneDescription(zoneJson.zoneDesc[zoneId].props)
+                    return (
+                        <li class={'zoneEntry'} id={`zoneEntry-${zoneId}`}>
+                            <button
+                                onclick={() => {
+                                    toggleInfoBox(zoneId)
+                                    goToZone(zoneId)
+                                }}
+                            >
+                                {zoneId}
+                            </button>
+                            <p class={'zoneInfoBox'}>{thisInfo}</p>
+                        </li>
+                    )
+                })}
+            </ul>
+        </>
     )
     return renderer.render(jsx).on(zoneListElement)
 }
 
 const zoneDescription = (zoneProps: ZoneSettings): string => {
-    if (!zoneProps) return ''
+    if (!zoneProps) return 'No properties'
     const keys = Object.keys(zoneProps)
     const string = keys.filter((key) => key.startsWith('Zone')).toString()
     const multiline = string.replace(',', ',\n')
@@ -45,7 +52,7 @@ const zoneDescription = (zoneProps: ZoneSettings): string => {
 }
 
 const toggleInfoBox = (zoneId: string) => {
-    const zoneInfoBox = document.getElementById(`infobox-${zoneId}`)
+    const zoneInfoBox = document.querySelector(`#zoneEntry-${zoneId}`)
     if (!zoneInfoBox) return
 
     const otherInfoBoxes = document.querySelectorAll('.zone-info-open')
@@ -54,4 +61,15 @@ const toggleInfoBox = (zoneId: string) => {
     })
 
     zoneInfoBox.classList.add('zone-info-open')
+}
+
+const filterZones = (input: string, zoneIDList: string[]) => {
+    for (const zoneId of zoneIDList) {
+        const zoneEntry = document.querySelector(`#zoneEntry-${zoneId}`)
+        if (zoneId.toLowerCase().includes(input.toLowerCase())) {
+            zoneEntry?.removeAttribute('hidden')
+        } else {
+            zoneEntry?.setAttribute('hidden', 'true')
+        }
+    }
 }
