b3a4ae33bd05b9354f2b16c39f43927137f34c39
Your Name <you@example.com>
2023-02-18 15:42:11 +0000

    Add wetness maps and use them for terrain specularity- much more accurate water rendering but some aesthetic details are lost

--- src/client/pathUtils.ts
+++ src/client/pathUtils.ts
@@ -5,7 +5,7 @@ export const terrainImagePath = (levelFileName: string): string => {
     return `./terrainimages/${levelFileName}_map.png`
 }
 export const tintImagePath = (levelFileName: string): string => {
-    return `./tint/${levelFileName}_tint_map__cmp.png`
+    return `./tint/${levelFileName}_tint_map__cmp_alpha.png`
 }
 export const mapZonesJsonPath = (levelFileName: string, versionSuffix: string = ''): string => {
     return `./mapZones/mapzones${levelFileName}.sso${versionSuffix}.json`
