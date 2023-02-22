b510e147fe7447e56de344c953f8360da1752cb3
Your Name <you@example.com>
2023-02-22 02:03:41 +0000

    Attempt to remove memory leak from Terrain

--- src/client/terrain.tsx
+++ src/client/terrain.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import * as THREE from 'three'
+import { PlaneGeometry } from 'three/src/geometries/PlaneGeometry'
 import { LevelJson } from '../typings/types'
 import { LAYERS } from './client'
 
@@ -18,32 +18,34 @@ export default function Terrain({
     snowTexture,
     levelJson,
 }: TerrainProps) {
+    const geomRef = React.useRef<PlaneGeometry>(null!)
     const mapSize = levelJson.mapSize
 
-    const geometry = new THREE.PlaneGeometry(
-        mapSize.mapX,
-        mapSize.mapZ,
-        mapSize.pointsX - 1,
-        mapSize.pointsZ - 1
-    )
-    geometry.name = 'terraingeom'
+    /* Work with the heightmap in a useeffect so we can clean up afterwards.
+     * Potential pitfall with the rotation being set in useeffect, but provided the planeGeometry
+     *  has a unique key this should be run exactly once per level
+     * */
+    React.useEffect(() => {
+        geomRef.current.rotateX(-Math.PI / 2) // flat plane
 
-    geometry.rotateX(-Math.PI / 2) // flat plane
-
-    const vertices = geometry.attributes.position
-    for (let i = 0; i < vertices.count; i++) {
-        const MAGIC_SCALING_FACTOR = mapSize.mapHeight / 256
-        vertices.setY(i, levelJson.heightMapList[i] * MAGIC_SCALING_FACTOR)
-    }
+        const vertices = geomRef.current.attributes.position
+        for (let i = 0; i < vertices.count; i++) {
+            const MAGIC_SCALING_FACTOR = mapSize.mapHeight / 256
+            vertices.setY(i, levelJson.heightMapList[i] * MAGIC_SCALING_FACTOR)
+        }
+        return () => {
+            geomRef.current?.dispose()
+        }
+    }, [levelJson])
 
     return (
-        <mesh
-            receiveShadow
-            castShadow
-            name="terrainMesh"
-            geometry={geometry}
-            layers={LAYERS.Terrain}
-        >
+        <mesh receiveShadow castShadow name="terrainMesh" layers={LAYERS.Terrain}>
+            <planeGeometry
+                key={levelJson.zones[0].name}
+                args={[mapSize.mapX, mapSize.mapZ, mapSize.pointsX - 1, mapSize.pointsZ - 1]}
+                ref={geomRef}
+                name={'terraingeom'}
+            />
             <meshPhongMaterial
                 name={'terrainMaterial'}
                 map={levelTexture}
