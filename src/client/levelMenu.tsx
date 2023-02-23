f34e67d58854de15f8eb9206c84e778fdb2228eb
Your Name <you@example.com>
2023-02-23 14:53:07 +0000

    More levels

--- src/client/levelMenu.tsx
+++ src/client/levelMenu.tsx
@@ -6,18 +6,34 @@ type LevelMenuProps = {
     pickLevel: (levelId: string) => void
 }
 
-const testLevelList = ['level_us_01_01', 'level_us_01_02', 'level_us_01_03', , 'level_us_01_04_new']
+const michigan = ['level_us_01_01', 'level_us_01_02', 'level_us_01_03', , 'level_us_01_04_new']
+const don = ['level_ru_05_01', 'level_ru_05_02']
+const amur = ['level_ru_04_01', 'level_ru_04_02', 'level_ru_04_03', , 'level_ru_04_04']
 export default function LevelMenu({ pickLevel }: LevelMenuProps) {
     return (
         <div id="levelMenu" className="levelMenuContainer">
             <div className="levelList">
-                {testLevelList.map((levelId) => {
-                    return (
+                <span>
+                    {michigan.map((levelId) => (
                         <button key={levelId} onClick={() => pickLevel(levelId!)}>
                             {levelId}
                         </button>
-                    )
-                })}
+                    ))}
+                </span>
+                <span>
+                    {don.map((levelId) => (
+                        <button key={levelId} onClick={() => pickLevel(levelId!)}>
+                            {levelId}
+                        </button>
+                    ))}
+                </span>
+                <span>
+                    {amur.map((levelId) => (
+                        <button key={levelId} onClick={() => pickLevel(levelId!)}>
+                            {levelId}
+                        </button>
+                    ))}
+                </span>
             </div>
         </div>
     )
